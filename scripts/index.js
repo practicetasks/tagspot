a55a4384d50c70cb35ecf298bfe016a16a323c76
Amir <a.nurmagambetov97@gmail.com>
2025-05-23 15:20:42 +0500

    second-commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,13 @@
-// @todo: Темплейт карточки
+const page = document.querySelector(".page");
+
 const cardContainer = document.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 const card = cardTemplate.querySelector(".card").cloneNode(true);
+const form = document.forms["new-place"];
+
+const newCardButton = document.querySelector(".profile__add-button");
+const popupNew = document.querySelector(".popup_type_new-card");
+const popupClose = popupNew.querySelector(".popup__close");
 
 function createInitialCard(array) {
   array.forEach((element) => {
@@ -17,30 +23,52 @@ function createInitialCard(array) {
     cardTitle.textContent = element.name;
     cardImage.src = element.link;
     cardContainer.append(newCard);
-    console.log(newCard);
 
     /* кнопка удаления */
 
-    buttonDelete.addEventListener("click", () => {
-      console.log("click");
-      newCard.remove();
-    });
+    page.addEventListener("click", (evt) => {
+      if (evt.target === buttonDelete) {
+        newCard.remove();
+      }
 
-    /* кнопка лайка */
+      if (evt.target === newCardButton) {
+        popupNew.classList.add("popup_is-opened");
+      }
 
-    buttonLike.addEventListener("click", () => {
-      console.log("click");
-      buttonLike.classList.toggle('card__like-button_is-active')
+      /* кнопка лайка */
+
+      if (evt.target === buttonLike) {
+        buttonLike.classList.toggle("card__like-button_is-active");
+      }
+
+      /* кнопка для закрытия окна создания поста */
+
+      if (evt.target === popupClose) {
+        closeWindow();
+      }
     });
   });
 }
 
 createInitialCard(initialCards);
 
-// @todo: DOM узлы
+function closeWindow() {
+  popupNew.classList.remove("popup_is-opened");
+}
 
-// @todo: Функция создания карточки
+/* функция добавления поста */
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
 
-// @todo: Функция удаления карточки
+  const newCard = card.cloneNode(true);
+  const cardImage = newCard.querySelector(".card__image");
+  const cardTitle = newCard.querySelector(".card__title");
 
-// @todo: Вывести карточки на страницу
+  cardImage.src = form.link.value;
+  cardTitle.textContent = form.placeName.value;
+
+  cardContainer.prepend(newCard);
+
+  closeWindow();
+  form.reset();
+});
