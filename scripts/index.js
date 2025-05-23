d4631668d08e2dc2cf458c1630a96e3440517558
Amir <a.nurmagambetov97@gmail.com>
2025-05-23 17:02:50 +0500

    3-commit

--- scripts/index.js
+++ scripts/index.js
@@ -9,48 +9,50 @@ const newCardButton = document.querySelector(".profile__add-button");
 const popupNew = document.querySelector(".popup_type_new-card");
 const popupClose = popupNew.querySelector(".popup__close");
 
+const buttonDelete = card.querySelector(".card__delete-button");
+const buttonLike = card.querySelector(".card__like-button");
+
 function createInitialCard(array) {
   array.forEach((element) => {
     /* переменные */
     const newCard = card.cloneNode(true);
     const cardImage = newCard.querySelector(".card__image");
     const cardTitle = newCard.querySelector(".card__title");
-    const buttonDelete = newCard.querySelector(".card__delete-button");
-    const buttonLike = newCard.querySelector(".card__like-button");
 
     /* генерация карточек */
 
     cardTitle.textContent = element.name;
     cardImage.src = element.link;
     cardContainer.append(newCard);
-
-    /* кнопка удаления */
-
-    page.addEventListener("click", (evt) => {
-      if (evt.target === buttonDelete) {
-        newCard.remove();
-      }
-
-      if (evt.target === newCardButton) {
-        popupNew.classList.add("popup_is-opened");
-      }
-
-      /* кнопка лайка */
-
-      if (evt.target === buttonLike) {
-        buttonLike.classList.toggle("card__like-button_is-active");
-      }
-
-      /* кнопка для закрытия окна создания поста */
-
-      if (evt.target === popupClose) {
-        closeWindow();
-      }
-    });
   });
 }
 
 createInitialCard(initialCards);
+
+/* кнопка удаления */
+page.addEventListener("click", (evt) => {
+  console.log(evt.target);
+  if (evt.target.classList.contains("card__delete-button")) {
+    console.log("clickDelete");
+    console.dir(evt.target)
+    evt.target.parentElement.remove()
+  }
+
+  /* кнопка лайка */
+
+  if (evt.target.classList.contains("card__like-button")) {
+    evt.target.classList.toggle("card__like-button_is-active");
+  }
+
+  if (evt.target === newCardButton) {
+    popupNew.classList.add("popup_is-opened");
+  }
+  /* кнопка для закрытия окна создания поста */
+
+  if (evt.target === popupClose) {
+    closeWindow();
+  }
+});
 
 function closeWindow() {
   popupNew.classList.remove("popup_is-opened");
